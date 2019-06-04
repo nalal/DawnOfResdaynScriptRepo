3a068c6e89cd36fa0871600ebd813480b671d8e2
Nac <noahgman11@gmail.com>
2019-06-04 16:01:32 -0600

    added created item

--- scripts/custom/basicCrafting/craftingRecipies.lua
+++ scripts/custom/basicCrafting/craftingRecipies.lua
@@ -7,7 +7,16 @@ local craftingRecipie = {
 				Ingred_Iron = 1,
 				Ingred_Leather = 1
 			},
-			diff = 0
+			diff = 2,
+			makes = "iron longsword"
+		},
+		pick_apprentice_01 = {
+			name = "Apprentice's Lockpic",
+			ingreds = {
+				Ingred_Bronze = 5
+			},
+			diff = 0,
+			makes = "pick_apprentice_01"
 		}
 	}
 }
