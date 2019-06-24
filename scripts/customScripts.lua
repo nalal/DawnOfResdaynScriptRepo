2ab39bd34979a667257883ae0970eddab1a2b51f
Nac <noahgman11@gmail.com>
2019-06-23 19:14:02 -0600

    Started, finished and committing RPChat rewrite

--- scripts/customScripts.lua
+++ scripts/customScripts.lua
@@ -1,10 +1,10 @@
 -- Internally developed scripts
-mysqlHandler = require("mysqlHandler")
-basicCraftingSkills = require("custom/basicCrafting/craftingData")
-basicCrafting = require("custom/basicCrafting/main")
-debugMode = require("custom/debugMode/main")
-basicNeeds = require("custom/basicNeeds/main")
-basicNeeds = require("custom/bankBuddy/main")
+--mysqlHandler = require("mysqlHandler")
+--basicCraftingSkills = require("custom/basicCrafting/craftingData")
+--basicCrafting = require("custom/basicCrafting/main")
+--debugMode = require("custom/debugMode/main")
+--basicNeeds = require("custom/basicNeeds/main")
+--basicNeeds = require("custom/bankBuddy/main")
 
 -- 3rd party scripting, thanks to those who are responsible
 FossMail = require("custom/FossMail/main")
